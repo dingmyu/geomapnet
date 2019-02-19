4e61e9409d040888aef916e0d70ef8f9a44e5615
dingmingyu <dingmingyu@sensetime.com>
2019-02-19 22:35:10 +0800

    multigpu

---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+now=`date +%Y-%m-%d,%H:%m:%s`
+srun -p bj11part --gres=gpu:4 -n1 --job-name=MapNet_fire python train.py --dataset 7Scenes --scene fire --config_file configs/posenet.ini --model posenet --device 0,1,2,3 --learn_beta --learn_gamma 2>&1|tee logs/train-${now}.log &
