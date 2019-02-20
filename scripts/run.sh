c36185a9e4a534e001513fa289776eca2e44b30a
dingmingyu <dingmingyu@sensetime.com>
2019-02-20 11:28:04 +0800

    run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,5 @@
 now=`date +%Y-%m-%d,%H:%m:%s`
-srun -p bj11part --gres=gpu:4 -n1 --job-name=MapNet_fire python train.py --suffix res18 --dataset 7Scenes --scene fire --config_file configs/posenet.ini --model posenet --device 0,1,2,3 --learn_beta --learn_gamma 2>&1|tee logs/train-${now}.log &
+suffix=res18
+scene=fire
+srun -p bj11part --gres=gpu:4 -n1 --job-name=MapNet_fire python train.py --suffix ${suffix} --dataset 7Scenes --scene ${scene} --config_file configs/posenet.ini --model posenet --device 0,1,2,3 --learn_beta --learn_gamma 2
+>&1|tee logs/train-${scene}-${suffix}-${now}.log &
