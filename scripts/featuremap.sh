ab8546a08b53ebfb8e57cf85f15528fbab4d1e0c
dingmingyu <dingmingyu@sensetime.com>
2019-02-22 22:33:01 +0800

    featuremap

---
+++ scripts/featuremap.sh
@@ -0,0 +1,5 @@
+now=`date +%Y-%m-%d,%H:%m:%s`
+name=7Scenes_fire_posenet_posenet_learn_beta_learn_gamma
+nohup srun -p bj11part --gres=gpu:1 -n1 --job-name=MapNet_fire python -u featuremap.py --dataset 7Scenes --scene fire --model posenet \
+--weights logs/${name}/epoch_300.pth.tar \
+--config_file configs/posenet.ini --val 2>&1|tee logs/eval-${name}-${now}.log &
